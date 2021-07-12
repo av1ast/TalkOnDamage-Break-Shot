5fac938f527bc65c9f7844a1c2a1752f2f6e61e0
CommunistDoggu <77682013+Jtinha2000@users.noreply.github.com>
2021-07-12 20:24:29 -0300

    Need be fixed

---
+++ DieSounds/DieSounds/Comeco.cs
@@ -0,0 +1,120 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Rocket.Unturned.Events;
+using Rocket.Core.Logging;
+using UnityEngine;
+using Logger = Rocket.Core.Logging.Logger;
+using System.Threading.Tasks;
+using SDG.NetTransport;
+using Rocket.Unturned.Chat;
+
+namespace Bootleg_Gritos
+{
+    public class Mun : RocketPlugin<Config>
+    {
+        public static Mun Instance { get; set; }
+        protected override void Load()
+        {
+            Instance = this;
+            Logger.Log("         Carregado", System.ConsoleColor.DarkRed);
+            Logger.Log("------------------------------", System.ConsoleColor.DarkRed);
+            Logger.Log("|                             |", System.ConsoleColor.DarkRed);
+            Logger.Log("|      By: CommunistDog       |", System.ConsoleColor.DarkRed);
+            Logger.Log("|     SoundEffect Plugins     |", System.ConsoleColor.DarkRed);
+            Logger.Log("|                             |", System.ConsoleColor.DarkRed);
+            Logger.Log("------------------------------", System.ConsoleColor.DarkRed);
+            Logger.Log("         Carregado", System.ConsoleColor.DarkRed);
+            UnturnedPlayerEvents.OnPlayerUpdateBleeding += UnturnedPlayerEvents_OnPlayerUpdateBleeding;
+            UnturnedPlayerEvents.OnPlayerUpdateBroken += UnturnedPlayerEvents_OnPlayerUpdateBroken;
+            UseableGun.onBulletHit += UseableGun_onBulletHit;
+        }
+
+        private void UseableGun_onBulletHit(UseableGun gun, BulletInfo bullet, InputInfo hit, ref bool shouldAllow)
+        {
+            System.Random rnd = new System.Random();
+            int Index = rnd.Next(0, Mun.Instance.Configuration.Instance.StartGunShotEffects.Count - 1);
+            EffectManager.sendEffect((ushort)Mun.Instance.Configuration.Instance.StartBreakLegEffects[Index].Effect, Mun.Instance.Configuration.Instance.StartBreakLegRadius, new Vector3(hit.player.movement.region_x, hit.player.movement.region_y));
+        }
+
+        private void UnturnedPlayerEvents_OnPlayerUpdateBroken(UnturnedPlayer player, bool broken)
+        {
+            if (broken == true && Mun.Instance.Configuration.Instance.HasStartBreakLegEffects == true)
+            {
+                System.Random rnd = new System.Random();
+                int Index = rnd.Next(0, Mun.Instance.Configuration.Instance.StartBreakLegEffects.Count - 1);
+                EffectManager.sendEffect((ushort)Mun.Instance.Configuration.Instance.StartBreakLegEffects[Index].Effect, Mun.Instance.Configuration.Instance.StartBreakLegRadius, player.Position);
+            }
+            else if (broken == false && Mun.Instance.Configuration.Instance.HasStopBreakLegEffects == true)
+            {
+                System.Random rnd = new System.Random();
+                int Index = rnd.Next(0, Mun.Instance.Configuration.Instance.StopBreakLegEffects.Count - 1);
+                EffectManager.sendEffect((ushort)Mun.Instance.Configuration.Instance.StopBreakLegEffects[Index].Effect, Mun.Instance.Configuration.Instance.StopBreakLegRadius, player.Position);
+
+            }
+            else if (broken == true && Mun.Instance.Configuration.Instance.HasWhileBreakLegEffects == true)
+            {
+                while (broken == true)
+                {
+                    System.Random rnd = new System.Random();
+                    int Index = rnd.Next(0, Mun.Instance.Configuration.Instance.WhileBreakLegEffects.Count - 1);
+                    Task.Delay((int)Mun.Instance.Configuration.Instance.WhileBreakLegIntervals * 1000);
+                    EffectManager.sendEffect((ushort)Mun.Instance.Configuration.Instance.StartBreakLegEffects[Index].Effect, Mun.Instance.Configuration.Instance.WhileBreakLegRadius, player.Position);
+                }
+
+            }
+            else
+            {
+
+            }
+        }
+
+        private void UnturnedPlayerEvents_OnPlayerUpdateBleeding(UnturnedPlayer player, bool bleeding)
+        {
+            EffectManager.sendEffect((ushort)Mun.Instance.Configuration.Instance.StartBleedingEffects[0].Effect, Mun.Instance.Configuration.Instance.StopBleedingRadius, player.Position);
+            if (bleeding == true && Mun.Instance.Configuration.Instance.HasStartBleedingEffects == true)
+            {
+                System.Random rnd = new System.Random();
+                int Index = rnd.Next(0, Mun.Instance.Configuration.Instance.StartBleedingEffects.Count - 1);
+                EffectManager.sendEffect((ushort)Mun.Instance.Configuration.Instance.StartBleedingEffects[Index].Effect, Mun.Instance.Configuration.Instance.StartBleedingRadius, player.Position);
+            }
+            else if (bleeding == false && Mun.Instance.Configuration.Instance.HasStopBleedingEffects == true)
+            {
+                System.Random rnd = new System.Random();
+                int Index = rnd.Next(0, Mun.Instance.Configuration.Instance.StopBleedingEffects.Count - 1);
+                EffectManager.sendEffect((ushort)Mun.Instance.Configuration.Instance.StartBleedingEffects[Index].Effect, Mun.Instance.Configuration.Instance.StopBleedingRadius, player.Position);
+
+            }
+            else if (bleeding == true && Mun.Instance.Configuration.Instance.HasWhileBleedingEffects == true)
+            {
+                while (bleeding == true)
+                {
+                    System.Random rnd = new System.Random();
+                    int Index = rnd.Next(0, Mun.Instance.Configuration.Instance.WhileBleedingEffects.Count - 1);
+                    Task.Delay((int)Mun.Instance.Configuration.Instance.WhileBleedingIntervals * 1000);
+                    EffectManager.sendEffect((ushort)Mun.Instance.Configuration.Instance.StartBleedingEffects[Index].Effect, Mun.Instance.Configuration.Instance.WhileBleedingRadius, player.Position);
+                }
+
+            }
+            else
+            {
+
+            }
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log("         Descarregado", System.ConsoleColor.DarkRed);
+            Logger.Log("------------------------------", System.ConsoleColor.DarkRed);
+            Logger.Log("|                             |", System.ConsoleColor.DarkRed);
+            Logger.Log("|      By: CommunistDog       |", System.ConsoleColor.DarkRed);
+            Logger.Log("|     SoundEffect Plugins     |", System.ConsoleColor.DarkRed);
+            Logger.Log("|                             |", System.ConsoleColor.DarkRed);
+            Logger.Log("------------------------------", System.ConsoleColor.DarkRed);
+            Logger.Log("         Descarregado", System.ConsoleColor.DarkRed);
+            UnturnedPlayerEvents.OnPlayerUpdateBleeding -= UnturnedPlayerEvents_OnPlayerUpdateBleeding;
+            UnturnedPlayerEvents.OnPlayerUpdateBroken -= UnturnedPlayerEvents_OnPlayerUpdateBroken;
+            UseableGun.onBulletHit -= UseableGun_onBulletHit;
+        }
+    }
+}
---
+++ DieSounds/DieSounds/Config.cs
@@ -0,0 +1,60 @@
+﻿using Rocket.API;
+using System.Collections.Generic;
+using Bootleg_Gritos.Entities;
+
+namespace Bootleg_Gritos
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public bool HasStartBleedingEffects { get; set; }
+
+        public List<Effects> StartBleedingEffects = new List<Effects>();
+        public int StartBleedingRadius { get; set; }
+        public bool HasWhileBleedingEffects { get; set; }
+        public List<Effects> WhileBleedingEffects = new List<Effects>();
+        public int WhileBleedingRadius { get; set; }
+        public double WhileBleedingIntervals { get; set; }
+        public bool HasStopBleedingEffects { get; set; }
+        public int StopBleedingRadius { get; set; }
+        public List<Effects> StopBleedingEffects = new List<Effects>();
+        public bool HasStartBreakLegEffects { get; set; }
+        public List<Effects> StartBreakLegEffects = new List<Effects>();
+        public int StartBreakLegRadius { get; set; }
+        public bool HasWhileBreakLegEffects { get; set; }
+        public List<Effects> WhileBreakLegEffects = new List<Effects>();
+        public int WhileBreakLegRadius { get; set; }
+        public double WhileBreakLegIntervals { get; set; }
+        public bool HasStopBreakLegEffects { get; set; }
+        public int StopBreakLegRadius { get; set; }
+        public List<Effects> StopBreakLegEffects = new List<Effects>();
+        public bool HasStartGunShotEffects { get; set; }
+        public List<Effects> StartGunShotEffects = new List<Effects>();
+        public int StartGunShotRadius { get; set; }
+        public void LoadDefaults()
+        {
+            HasStartBleedingEffects = true;
+            StartBleedingRadius = 50;
+            StartBleedingEffects.Add(new Effects(54110));
+            HasWhileBleedingEffects = true;
+            WhileBleedingRadius = 50;
+            WhileBleedingIntervals = 30;
+            WhileBleedingEffects.Add(new Effects(5411));
+            HasStopBleedingEffects = true;
+            StopBleedingRadius = 50;
+            StopBleedingEffects.Add(new Effects(5411));
+            HasStartBreakLegEffects = true;
+            StartBreakLegRadius = 50;
+            StartBreakLegEffects.Add(new Effects(5411));
+            HasWhileBreakLegEffects = true;
+            WhileBreakLegRadius = 50;
+            WhileBreakLegIntervals = 30;
+            WhileBreakLegEffects.Add(new Effects(5411));
+            HasStopBreakLegEffects = true;
+            StopBreakLegRadius = 50;
+            StopBreakLegEffects.Add(new Effects(5411));
+            HasStartGunShotEffects = true;
+            StartGunShotRadius = 50;
+            StartGunShotEffects.Add(new Effects(5411));
+        }
+    }
+}
